cdffe5ad9e37b9317e3448fd7f894eb15646cb1f
Ashwant R <mohamedlahir1234@gmail.com>
2023-10-31 23:18:59 +0530

    lahir

--- MajorityOfAnElement.java
+++ MajorityOfAnElement.java
@@ -2,33 +2,17 @@ import java.util.LinkedList;
 
 //https://leetcode.com/problems/majority-element/
 public class MajorityOfAnElement {
-    public int findMajorityElement(int[] nums) {
-        int ans = 0;
-        int length = nums.length;
-
-        int n = (length / 2);
-//        System.out.println(n);
-        int count = 0;
-            for (int i = 0; i < nums.length; i++) {
-                count = 0;
-                for (int j = 0; j < nums.length; j++) {
-                    if (nums[i] == nums[j]) {
-                        count++;
-                        if (count > n) {
-                            ans = nums[i];
-                            return ans;
-                        }
-                    }
-                }
-            }
+    public int findMajority(int nums[]){
+        int ans=0;
+int count=0;
+int length=0;
 
 
-            return ans;
+        return ans;
     }
     public static void main(String[] args) {
         int[] nums = {6,5,5};
         MajorityOfAnElement obj = new MajorityOfAnElement();
-        System.out.println(obj.findMajorityElement(nums));
-//        LinkedList<String> cars = new LinkedList<String>();
+        System.out.println(obj.findMajority(nums));
     }
 }
